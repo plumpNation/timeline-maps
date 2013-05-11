c3618cdc8b5c62dc969e44a9e5fd5cdf85a83a9d
Gavin King <gavin.king@fronter.com>
2013-05-11 15:30:27 +0200

    [Added function from the fresh tutorial to try manipulate end point.]

--- scripts/main.js
+++ scripts/main.js
@@ -4,15 +4,42 @@ var params = {
 
     assets = {
         'location': 'assets/',
-        'map': 'europe'
+        'map': 'star'
     },
-
-    assetContainer = document.getElementById('assets'),
 
     container = document.getElementById('app-container'),
 
-    two = new Two().appendTo(container);
+    two = new Two().appendTo(container)
 
-$(assetContainer).load(assets.location + assets.map + '.svg');
+    /**
+     * [setEnding description]
+     *
+     * @tutorial http://jonobr1.github.io/two.js/examples/animate-stroke.html
+     * @param {svg} group
+     * @param {number} t
+     */
+    setEnding = function (group, t) {
+
+        var i = 0,
+            traversed = t * group.total,
+            current = 0;
+
+            _(group.children).each(function (child) {
+                var distance = group.distances[i],
+                    min = current,
+                    max = current + distance,
+                    pct = cmap(traversed, min, max, 0, 1);
+
+                child.ending = pct;
+                current = max;
+                i++;
+            }
+        );
+    };
+
+$.get(assets.location + assets.map + '.svg').then(function (doc) {
+    var svg = $(doc).find('svg')[0],
+        interpretedSvg = two.interpret(svg);
+});
 
 two.update();
