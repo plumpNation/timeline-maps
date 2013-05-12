3f22029360b6d71de51042f13e6b90189db2a8e0
Gavin King <gavin.king@fronter.com>
2013-05-12 17:47:23 +0200

    [Experimenting with inkscape export features and Two interpret]

--- scripts/main.js
+++ scripts/main.js
@@ -4,15 +4,20 @@ var params = {
 
     assets = {
         'location': 'assets/',
-        'map': 'europe'
+        'map': 'triangleInk'
     },
-
-    assetContainer = document.getElementById('assets'),
 
     container = document.getElementById('app-container'),
 
-    two = new Two().appendTo(container);
+    two = new Two().appendTo(container),
 
-$(assetContainer).load(assets.location + assets.map + '.svg');
+    map;
+
+$.get(assets.location + assets.map + '.svg').then(function (doc) {
+    // an svg is in fact a document so we need to extract the actual svg tag from it.
+    var data = $(doc).find('svg')[0];
+    map = two.interpret(data);
+    /*$(container).append(map);*/
+});
 
 two.update();
