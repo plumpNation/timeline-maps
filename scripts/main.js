7e9832033a730e059faf085b547cf2117a3ce98b
Gavin King <gavin.king@fronter.com>
2013-05-25 01:33:32 +0200

    [Working with multiple clicks then draw]

--- scripts/main.js
+++ scripts/main.js
@@ -2,17 +2,47 @@ var params = {
         'fullscreen': true,
     },
 
-    assets = {
-        'location': 'assets/',
-        'map': 'europe'
-    },
-
-    assetContainer = document.getElementById('assets'),
+    points = [],
 
     container = document.getElementById('app-container'),
 
-    two = new Two().appendTo(container);
+    two = new Two().appendTo(container),
 
-$(assetContainer).load(assets.location + assets.map + '.svg');
+    drawCurve = function (e) {
+        var pathClosed  = false,
+            args        = _.flatten(points),
+            path;
+
+        path = two.makeCurve.call(two, args, !pathClosed);
+
+        path.fill = 'none';
+        // two.update();
+    },
+
+    reportLocation = function (x, y) {
+        $('#header2').html(
+            'X-Position:' + x + ' Y-Position:' + y);
+    },
+
+    storePoint = function (x, y) {
+        var point = new Two.Vector(x, y);
+        points.push(point);
+        reportLocation(x, y);
+    },
+
+    recordClickLocation = function (e) {
+        var parentOffset = $(this).parent().offset(),
+            // offset -> method allows you to retrieve the current position of an
+            // element 'relative' to the document.
+            x = (e.pageX - parentOffset.left),
+            y = (e.pageY - parentOffset.top);
+
+        storePoint(x, y);
+
+        reportLocation(x, y);
+    };
+
+$(container).on('click', recordClickLocation);
+$('#draw-curve').on('click', drawCurve);
 
 two.update();
