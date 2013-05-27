62648e8fc5431cf2342ae1436798f1308ceeb7a7
Gavin King <gavin.king@fronter.com>
2013-05-27 17:51:59 +0200

    [DOM scripting for animation path]

--- scripts/main.js
+++ scripts/main.js
@@ -1,45 +1,84 @@
-var params = {
+var params    = {
         width: '100%',
         height: '100%'
     },
 
-    points = [],
+    ns        = 'http://www.w3.org/2000/svg',
+    ns2       = 'http://www.w3.org/1999/xlink',
+
+    prefix    = '#two-',
 
     container = document.getElementById('app-container'),
+    two       = new Two(params).appendTo(container),
 
-    two = new Two(params).appendTo(container),
+    points    = [],
+    dotsGroup,
 
-    clearCircles = function () {
-        $('.circle').remove();
+    curvesGroupElement,
+    curvesGroup,
+
+
+    clearDots = function () {
+        two.remove(dotsGroup);
+        dotsGroup = null;
     },
 
+    /**
+     * Adds a small polygon and animates it along the path.
+     *
+     * @param {Two.Polygon} path The path to use as a reference.
+     *
+     * @see http://jsfiddle.net/FjRZA/
+     *
+     * Output looks somthing like this hopefully:
+     * <path id="cubicCurve" d="..."></path>
+     * <circle cx="" cy="" r="5" fill="red">
+     *     <animateMotion dur="6s" rotate="auto" fill="freeze">
+     *         <mpath xlink:href="#cubicCurve"/>
+     *     </animateMotion>
+     * </circle>
+     */
     addPathAnimation = function (path) {
-        var anim = '<circle cx="" cy="" r="45" fill="blue">' +
 
-                '<!-- Define the motion path animation -->' +
-                '<animateMotion dur="6s" repeatCount="indefinite">' +
-                   '<mpath xlink:href="#two-' + path.id + '"/>' +
-                '</animateMotion>' +
-            '</circle>';
+        var animateMotion = document.createElementNS(ns, 'animateMotion'),
+            mpath         = document.createElementNS(ns, 'mpath'),
+            circle        = document.createElementNS(ns, 'circle'),
+            pathId        = '#two-' + path.id,
+            startVertex   = path.vertices[0];
 
-        $(container).find('svg g').append(anim);
-        Two.Utils.setPlaying(true);
+        console.log('Adding path animation');
+
+        curvesGroupElement.appendChild(circle);
+
+        circle.setAttribute('cx'          , startVertex.x);
+        circle.setAttribute('cy'          , startVertex.y);
+        circle.setAttribute('r'           , '60');
+        circle.setAttribute('fill'        , '#336699');
+        circle.setAttribute('stroke-width', '5');
+        circle.setAttribute('stroke'      , '#FF0000');
+
+        animateMotion.setAttribute('dur'        , '6s');
+        animateMotion.setAttribute('rotate'     , 'auto');
+        animateMotion.setAttribute('repeatCount', 'indefinite');
+
+        mpath.setAttributeNS(ns2, 'href', pathId);
+        animateMotion.appendChild(mpath);
+
+        circle.appendChild(animateMotion);
     },
 
     takePicture = function (e) {
         // WebActivities
         var pick = new MozActivity({
-            name: "pick",
+            name: 'pick',
             data: {
-                type: ["image/png", "image/jpg", "image/jpeg"]
+                type: ['image/png', 'image/jpg', 'image/jpeg']
             }
         });
 
         pick.onsuccess = function () { 
             var piccyUrl = window.URL.createObjectURL(this.result.blob);
-            $(container).css({
-                'background-image': piccyUrl
-            });
+            container.style.backgroundImage = piccyUrl;
         };
 
         pick.onerror = function () { 
@@ -48,7 +87,20 @@ var params = {
     },
 
     clearPoints = function () {
+        clearDots();
         points = [];
+    },
+
+    drawCurveThrough = function (args, pathClosed) {
+        var path = two.makeCurve.call(two, args, !pathClosed);
+
+        path.fill      = 'none';
+        path.stroke    = 'red';
+        path.cap       = 'square';
+        path.linewidth = 10;
+
+        return path;
+
     },
 
     drawCurve = function (e) {
@@ -56,12 +108,20 @@ var params = {
             args        = _.flatten(points),
             path;
 
-        path = two.makeCurve.call(two, args, !pathClosed);
+        if (!points.length) {
+            return false;
+        }
 
-        path.fill = 'none';
-        path.stroke = 'red';
-        path.cap = 'square';
-        path.linewidth = 10;
+        if (!curvesGroup) {
+            curvesGroup = new Two.Group();
+            two.add(curvesGroup);
+            curvesGroupElement = $(prefix + curvesGroup.id)[0];
+        }
+
+        path = drawCurveThrough(args, pathClosed);
+
+        curvesGroup.add(path);
+        console.log(curvesGroup.id);
 
         clearPoints();
         return path;
@@ -73,8 +133,13 @@ var params = {
     },
 
     addDot = function (x, y) {
-        var circle = two.makeCircle(x, y, 3);
-        $(circle).addClass('circle');
+        var dot = two.makeCircle(x, y, 3);
+        if (!dotsGroup) {
+            dotsGroup = new Two.Group();
+            two.add(dotsGroup);
+        }
+
+        dotsGroup.add(dot);
     },
 
     recordClickLocation = function (e) {
@@ -105,8 +170,11 @@ var params = {
     },
 
     submitCurve = function () {
+        console.log('Making curve');
         var path = drawCurve();
-        addPathAnimation(path);
+        if (path) {
+            addPathAnimation(path);
+        }
     };
 
 $(container).on('click', recordClickLocation);
@@ -115,6 +183,10 @@ $('#draw-curve').on('click', submitCurve);
 if (typeof MozActivity === 'function') {
     setupForFirefoxOS();
 }
+
+two.on('update', function () {
+    console.log('updated');
+});
 
 // adds the application svg to the page
 two.update();
