e42166ee07dbc4741f39beaa512ff61fbcde3426
Gavin King <gavin.king@fronter.com>
2013-06-01 01:58:59 +0200

    [Stable drawing straight lines]

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,61 @@
-var params = {
-        'fullscreen': true,
+var pointsData = [],
+
+    sampleSVG = d3.select('#viz')
+        .append('svg')
+            .attr('width' , '100%')
+            .attr('height', '100%'),
+
+    dotsGroup = sampleSVG.append('g')
+            .attr('class', 'dots'),
+
+    curvesGroup = sampleSVG.append('g')
+            .attr('class', 'curves'),
+
+    addDot = function (x, y) {
+        dotsGroup.append('circle')
+            .attr({
+                'cx': x,
+                'cy': y,
+                'r' : 15
+            });
     },
 
-    assets = {
-        'location': 'assets/',
-        'map': 'europe'
+    storePoint = function (x, y) {
+        addDot(x, y);
+        pointsData.push({'x': x, 'y': y});
     },
 
-    assetContainer = document.getElementById('assets'),
+    getPointsAsString = function () {
+        var pointsString = '';
 
-    container = document.getElementById('app-container'),
+        for(var i = 0; i < pointsData.length; i += 1) {
+            pointsString += pointsData[i].x + ',' + pointsData[i].y + ' ';
+        }
 
-    two = new Two().appendTo(container);
+        return pointsString;
+    },
 
-$(assetContainer).load(assets.location + assets.map + '.svg');
+    drawCurve = function () {
+        curvesGroup.append('polyline')
+            .attr({
+                'points': getPointsAsString()
+            })
+            .style({
+                'fill'  : 'none',
+                'stroke': 'red',
+                'stroke-width': 5
+            });
 
-two.update();
+        pointsData = [];
+    };
+
+$('svg').on('click', function (e) {
+    var x = e.clientX,
+        y = e.clientY;
+
+    storePoint(x, y);
+});
+
+$('#draw').on('click', function () {
+    drawCurve();
+});
